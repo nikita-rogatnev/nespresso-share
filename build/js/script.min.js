608c46c0d3aab1707130a555c8ddcdf1ee7adcbb
Rogatnev Nikita <nikita.rogatnev@omnigon.com>
2018-05-04 21:12:22 +0300

    slider init

--- build/js/script.min.js
+++ build/js/script.min.js
@@ -2,3 +2,89 @@ $(".intro__slide").click(function (e) {
     e.preventDefault();
     $(".modal").removeClass('is-hidden');
 });
+
+
+$(".modal__close").click(function (e) {
+    e.preventDefault();
+    $(".modal").addClass('is-hidden');
+});
+
+
+$(document).ready(function () {
+
+    //rotation speed and timer
+    var speed = 5000;
+    var run = setInterval('rotate()', speed);
+
+    //grab the width and calculate left value
+    var item_width = $('#slides li').outerWidth();
+    var left_value = item_width * (-1);
+
+    //move the last item before first item, just in case user click prev button
+    $('.slider__slides .slider__slide:first').before($('.slider__slides .slider__slide:last'));
+
+    //set the default item to the correct position
+    $('.slider__slides').css({'left': left_value});
+
+    //if user clicked on prev button
+    $('#prev').click(function () {
+
+        //get the right position
+        var left_indent = parseInt($('.slider__slides').css('left')) + item_width;
+
+        //slide the item
+        $('.slider__slides').animate({'left': left_indent}, 200, function () {
+
+            //move the last item and put it as first item
+            $('.slider__slides .slider__slide:first').before($('.slider__slides .slider__slide:last'));
+
+            //set the default item to correct position
+            $('#slides ul').css({'left': left_value});
+
+        });
+
+        //cancel the link behavior
+        return false;
+
+    });
+
+
+    //if user clicked on next button
+    $('#next').click(function () {
+
+        //get the right position
+        var left_indent = parseInt($('.slider__slides').css('left')) - item_width;
+
+        //slide the item
+        $('.slider__slides').animate({'left': left_indent}, 200, function () {
+
+            //move the first item and put it as last item
+            $('.slider__slides .slider__slide:last').after($('.slider__slides .slider__slide:first'));
+
+            //set the default item to correct position
+            $('.slider__slides').css({'left': left_value});
+
+        });
+
+        //cancel the link behavior
+        return false;
+
+    });
+
+    //if mouse hover, pause the auto rotation, otherwise rotate it
+    $('#slides').hover(
+        function () {
+            clearInterval(run);
+        },
+        function () {
+            run = setInterval('rotate()', speed);
+        }
+    );
+
+});
+
+//a simple function to click next link
+//a timer will call this function, and the rotation will begin :)
+function rotate() {
+    $('#next').click();
+}
