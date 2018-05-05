975f96480fd26a9b80b73292b0146977ca358ad0
Rogatnev Nikita <nikita.rogatnev@omnigon.com>
2018-05-05 17:49:43 +0300

    styling

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,32 @@
-$(".intro__slide").click(function (e) {
-    e.preventDefault();
-    $(".modal").removeClass('is-hidden');
-});
-
-
-$(".modal__close").click(function (e) {
-    e.preventDefault();
-    $(".modal").addClass('is-hidden');
-});
-
-
 $(document).ready(function () {
+    $(".intro .slider__slide").click(function (e) {
+        e.preventDefault();
+        $(".modal").removeClass('is-hidden');
+        $(".slider").addClass('is-modal');
+    });
+
+    $(".modal__close").click(function (e) {
+        e.preventDefault();
+        $(".modal").addClass('is-hidden');
+        $(".slider").removeClass('is-modal is-hidden');
+
+    });
+
+    $(".slider__link a").click(function (e) {
+        e.preventDefault();
+        $(".slider").addClass('is-hidden');
+        $(".form").removeClass('is-hidden');
+    });
+
+
+
 
     //rotation speed and timer
     var speed = 5000;
     var run = setInterval('rotate()', speed);
 
     //grab the width and calculate left value
-    var item_width = $('#slides li').outerWidth();
+    var item_width = $('.slider__slide').outerWidth();
     var left_value = item_width * (-1);
 
     //move the last item before first item, just in case user click prev button
@@ -25,6 +34,7 @@ $(document).ready(function () {
 
     //set the default item to the correct position
     $('.slider__slides').css({'left': left_value});
+
 
     //if user clicked on prev button
     $('#prev').click(function () {
@@ -39,13 +49,12 @@ $(document).ready(function () {
             $('.slider__slides .slider__slide:first').before($('.slider__slides .slider__slide:last'));
 
             //set the default item to correct position
-            $('#slides ul').css({'left': left_value});
+            $('.slider__slides').css({'left': left_value});
 
         });
 
         //cancel the link behavior
         return false;
-
     });
 
 
@@ -71,20 +80,4 @@ $(document).ready(function () {
 
     });
 
-    //if mouse hover, pause the auto rotation, otherwise rotate it
-    $('#slides').hover(
-        function () {
-            clearInterval(run);
-        },
-        function () {
-            run = setInterval('rotate()', speed);
-        }
-    );
-
 });
-
-//a simple function to click next link
-//a timer will call this function, and the rotation will begin :)
-function rotate() {
-    $('#next').click();
-}
