87e06322f42b1f42395b9605d3de9ab27d6afe4f
Rogatnev Nikita <nikita.rogatnev@omnigon.com>
2018-05-22 16:53:48 +0300

    designs upd

--- src/js/script.js
+++ src/js/script.js
@@ -455,42 +455,29 @@ JSShare = {
 };
 
 $(document).ready(function () {
-    $('.owl-carousel').children().each(function (index) {
-        $(this).attr('data-position', index);
-    });
-
-    if ($(window).width() < 1160) {
-        $('.owl-carousel').owlCarousel({
-            center: true,
-            loop: true,
-            items: 1,
-        });
-    }
-
-    else {
-        $('.owl-carousel').owlCarousel({
-            center: true,
-            loop: true,
-            items: 3,
-        });
-    }
-
-    // Go to the next item
-    $('.owl-next').click(function () {
-        $('.owl-carousel').trigger('next.owl.carousel');
-    });
-
-    // Go to the previous item
-    $('.owl-prev').click(function () {
-        $('.owl-carousel').trigger('prev.owl.carousel', [300]);
-    });
-
-    $(document).on('click', '.owl-item>div', function () {
-        $('.owl-carousel').trigger('to.owl.carousel', $(this).data('position'));
-    });
-
-    $(".intro .slider__slide").click(function (e) {
+    $(".slider .slider__slide").click(function (e) {
         e.preventDefault();
+
+        $(".slider .slider__slide").removeClass('slider__slide--active');
+        $(this).addClass('slider__slide--active');
+    });
+
+    $(".slider__button--left").click(function (e) {
+        e.preventDefault();
+
+        $(".slider .slider__slide--active:not(:first-child)").removeClass('slider__slide--active').prev().addClass("slider__slide--active");
+    });
+
+    $(".slider__button--right").click(function (e) {
+        e.preventDefault();
+
+        $(".slider .slider__slide--active:not(:last-child)").removeClass('slider__slide--active').next().addClass("slider__slide--active");
+    });
+
+
+    $(".slider .slider__slidesss").click(function (e) {
+        e.preventDefault();
+
         var slideId = $('.center .slider__slide').attr('id');
         $(location).attr('href', slideId + '.html');
     });
